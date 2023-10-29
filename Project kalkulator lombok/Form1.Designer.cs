138b266f35244e109baf4038473563e52a685c21
Teheft1 <yunus.22081@mhs.unesa.ac.id>
2023-10-29 16:52:02 +0700

    Add project files.

---
+++ Project kalkulator lombok/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Project_kalkulator_lombok
+{
+    partial class Kalkulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.angk1 = new System.Windows.Forms.Button();
+            this.Hasil = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.angk2 = new System.Windows.Forms.Button();
+            this.angk3 = new System.Windows.Forms.Button();
+            this.angk4 = new System.Windows.Forms.Button();
+            this.angk5 = new System.Windows.Forms.Button();
+            this.angk6 = new System.Windows.Forms.Button();
+            this.angk7 = new System.Windows.Forms.Button();
+            this.angk8 = new System.Windows.Forms.Button();
+            this.angk9 = new System.Windows.Forms.Button();
+            this.angk0 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.btn_equal = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.KurangButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // angk1
+            // 
+            this.angk1.Location = new System.Drawing.Point(70, 204);
+            this.angk1.Name = "angk1";
+            this.angk1.Size = new System.Drawing.Size(75, 23);
+            this.angk1.TabIndex = 0;
+            this.angk1.Text = "1";
+            this.angk1.UseVisualStyleBackColor = true;
+            this.angk1.Click += new System.EventHandler(this.btn_click);
+            // 
+            // Hasil
+            // 
+            this.Hasil.Location = new System.Drawing.Point(70, 178);
+            this.Hasil.Name = "Hasil";
+            this.Hasil.Size = new System.Drawing.Size(237, 20);
+            this.Hasil.TabIndex = 1;
+            this.Hasil.Text = "0";
+            this.Hasil.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Hasil.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // angk2
+            // 
+            this.angk2.Location = new System.Drawing.Point(151, 204);
+            this.angk2.Name = "angk2";
+            this.angk2.Size = new System.Drawing.Size(75, 23);
+            this.angk2.TabIndex = 3;
+            this.angk2.Text = "2";
+            this.angk2.UseVisualStyleBackColor = true;
+            this.angk2.Click += new System.EventHandler(this.btn_click);
+            // 
+            // angk3
+            // 
+            this.angk3.Location = new System.Drawing.Point(232, 204);
+            this.angk3.Name = "angk3";
+            this.angk3.Size = new System.Drawing.Size(75, 23);
+            this.angk3.TabIndex = 4;
+            this.angk3.Text = "3";
+            this.angk3.UseVisualStyleBackColor = true;
+            this.angk3.Click += new System.EventHandler(this.btn_click);
+            // 
+            // angk4
+            // 
+            this.angk4.Location = new System.Drawing.Point(70, 233);
+            this.angk4.Name = "angk4";
+            this.angk4.Size = new System.Drawing.Size(75, 23);
+            this.angk4.TabIndex = 5;
+            this.angk4.Text = "4";
+            this.angk4.UseVisualStyleBackColor = true;
+            this.angk4.Click += new System.EventHandler(this.btn_click);
+            // 
+            // angk5
+            // 
+            this.angk5.Location = new System.Drawing.Point(151, 233);
+            this.angk5.Name = "angk5";
+            this.angk5.Size = new System.Drawing.Size(75, 23);
+            this.angk5.TabIndex = 6;
+            this.angk5.Text = "5";
+            this.angk5.UseVisualStyleBackColor = true;
+            this.angk5.Click += new System.EventHandler(this.btn_click);
+            // 
+            // angk6
+            // 
+            this.angk6.Location = new System.Drawing.Point(232, 233);
+            this.angk6.Name = "angk6";
+            this.angk6.Size = new System.Drawing.Size(75, 23);
+            this.angk6.TabIndex = 7;
+            this.angk6.Text = "6";
+            this.angk6.UseVisualStyleBackColor = true;
+            this.angk6.Click += new System.EventHandler(this.btn_click);
+            // 
+            // angk7
+            // 
+            this.angk7.Location = new System.Drawing.Point(70, 262);
+            this.angk7.Name = "angk7";
+            this.angk7.Size = new System.Drawing.Size(75, 23);
+            this.angk7.TabIndex = 8;
+            this.angk7.Text = "7";
+            this.angk7.UseVisualStyleBackColor = true;
+            this.angk7.Click += new System.EventHandler(this.btn_click);
+            // 
+            // angk8
+            // 
+            this.angk8.Location = new System.Drawing.Point(151, 262);
+            this.angk8.Name = "angk8";
+            this.angk8.Size = new System.Drawing.Size(75, 23);
+            this.angk8.TabIndex = 9;
+            this.angk8.Text = "8";
+            this.angk8.UseVisualStyleBackColor = true;
+            this.angk8.Click += new System.EventHandler(this.btn_click);
+            // 
+            // angk9
+            // 
+            this.angk9.Location = new System.Drawing.Point(232, 262);
+            this.angk9.Name = "angk9";
+            this.angk9.Size = new System.Drawing.Size(75, 23);
+            this.angk9.TabIndex = 10;
+            this.angk9.Text = "9";
+            this.angk9.UseVisualStyleBackColor = true;
+            this.angk9.Click += new System.EventHandler(this.btn_click);
+            // 
+            // angk0
+            // 
+            this.angk0.Location = new System.Drawing.Point(151, 291);
+            this.angk0.Name = "angk0";
+            this.angk0.Size = new System.Drawing.Size(75, 23);
+            this.angk0.TabIndex = 11;
+            this.angk0.Text = "0";
+            this.angk0.UseVisualStyleBackColor = true;
+            this.angk0.Click += new System.EventHandler(this.btn_click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(313, 204);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "+";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.op_click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(313, 233);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 13;
+            this.button12.Text = "-";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.op_click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(313, 262);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 14;
+            this.button13.Text = "*";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.op_click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(313, 291);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 23);
+            this.button14.TabIndex = 15;
+            this.button14.Text = "/";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.op_click);
+            // 
+            // btn_equal
+            // 
+            this.btn_equal.Location = new System.Drawing.Point(232, 291);
+            this.btn_equal.Name = "btn_equal";
+            this.btn_equal.Size = new System.Drawing.Size(75, 23);
+            this.btn_equal.TabIndex = 16;
+            this.btn_equal.Text = "=";
+            this.btn_equal.UseVisualStyleBackColor = true;
+            this.btn_equal.Click += new System.EventHandler(this.btn_equal_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(70, 291);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear.TabIndex = 17;
+            this.btn_clear.Text = "C";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // KurangButton
+            // 
+            this.KurangButton.Location = new System.Drawing.Point(313, 178);
+            this.KurangButton.Name = "KurangButton";
+            this.KurangButton.Size = new System.Drawing.Size(75, 23);
+            this.KurangButton.TabIndex = 18;
+            this.KurangButton.Text = "<<";
+            this.KurangButton.UseVisualStyleBackColor = true;
+            this.KurangButton.Click += new System.EventHandler(this.kurang_click);
+            // 
+            // Kalkulator
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 481);
+            this.Controls.Add(this.KurangButton);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_equal);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.angk0);
+            this.Controls.Add(this.angk9);
+            this.Controls.Add(this.angk8);
+            this.Controls.Add(this.angk7);
+            this.Controls.Add(this.angk6);
+            this.Controls.Add(this.angk5);
+            this.Controls.Add(this.angk4);
+            this.Controls.Add(this.angk3);
+            this.Controls.Add(this.angk2);
+            this.Controls.Add(this.Hasil);
+            this.Controls.Add(this.angk1);
+            this.Name = "Kalkulator";
+            this.Text = "Kalkulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button angk1;
+        private System.Windows.Forms.TextBox Hasil;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button angk2;
+        private System.Windows.Forms.Button angk3;
+        private System.Windows.Forms.Button angk4;
+        private System.Windows.Forms.Button angk5;
+        private System.Windows.Forms.Button angk6;
+        private System.Windows.Forms.Button angk7;
+        private System.Windows.Forms.Button angk8;
+        private System.Windows.Forms.Button angk9;
+        private System.Windows.Forms.Button angk0;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button btn_equal;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button KurangButton;
+    }
+}
+
---
+++ Project kalkulator lombok/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_kalkulator_lombok
+{
+    public partial class Kalkulator : Form
+    {
+        double value = 0;
+        string ops = "";
+        bool op_pressed = false;
+        public Kalkulator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_click(object sender, EventArgs e)
+        {
+            if (Hasil.Text == "0" || op_pressed)
+                Hasil.Clear();
+
+            op_pressed = false;
+
+            Button b = (Button)sender;
+            
+            Hasil.Text = Hasil.Text +  b.Text;
+        }
+
+        private void kurang_click(object sender, EventArgs e)
+        {
+            Hasil.Text = Hasil.Text.Remove(Hasil.Text.Length - 1, 1);
+            if (Hasil.Text == "")
+                Hasil.Text = "0";
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            Hasil.Clear();
+            Hasil.Text = "0";
+        }
+
+        private void btn_equal_Click(object sender, EventArgs e)
+        {
+            switch (ops)
+            {
+                case "+": Hasil.Text = (value + Double.Parse(Hasil.Text)).ToString();
+                    op_pressed = false;
+                    break;
+                case "-": Hasil.Text = (value - Double.Parse(Hasil.Text)).ToString();
+                    op_pressed = false;
+                    break;
+                case "*": Hasil.Text = (value * Double.Parse(Hasil.Text)).ToString();
+                    op_pressed = false;
+                    break;
+                case "/": Hasil.Text = (value / Double.Parse(Hasil.Text)).ToString();
+                    op_pressed = false;
+                    break;
+            }
+        }
+
+        private void op_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            ops = b.Text;
+            op_pressed = true;
+            value = Double.Parse(Hasil.Text);
+
+            op_pressed = true;
+        }
+    }
+}
